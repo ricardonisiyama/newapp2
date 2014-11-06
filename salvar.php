82e411cd87f6ee52a82e2eb022d753dc7b7316d8
Ricardo Nisiyama <ricardo@lionsoft.com.br>
2014-11-06 16:38:14 -0200

    Signed-off-by: Ricardo Nisiyama <ricardo@lionsoft.com.br>

---
+++ salvar.php
@@ -0,0 +1,20 @@
+﻿<?
+	
+	$con = mysql_connect('192.168.1.155','lionsoft','*lionsoft.2013$')or die(mysql_error());
+	mysql_select_db('phonegap_teste', $con);
+	
+	$nome = mysql_real_escape_string($_REQUEST['nome']);
+	$sobrenome = mysql_real_escape_string($_REQUEST['sobrenome']);
+	$email = mysql_real_escape_string($_REQUEST['email']);
+	
+	$sql = "INSERT INTO cadastro ( nome, sobrenome, email) VALUES('$nome', '$sobrenome', '$email')";
+	$res = mysql_query($sql);
+	
+	if($res == true){
+		$cadastro = 1;
+	}else{
+		$cadastro = 0;	
+	}
+	
+	echo (json_encode($cadastro));
+?>
